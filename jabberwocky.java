9d01f7f5a707adf8e8eeb93a34aa6ca1b93ff12b
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2019-09-06 09:12:46 -0700

    Added Jabberwocky

---
+++ jabberwocky.java
@@ -0,0 +1,5 @@
+public class jabberwocky  {
+    public static void main (String [] args)  {
+        System.out.print("’twas brillig, and the slithy toves/\n\tDid gyre and gimble in the wabe:\nAll mimsy were the \"borogroves\"/\n\tAnd the mome raths out grabe.");
+    }
+}
