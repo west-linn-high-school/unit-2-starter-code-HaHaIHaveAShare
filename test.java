d52878265a75b61873000f4741325701515bca65
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2019-09-09 08:42:52 -0700

    I'm disabled

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
-public static test {
-    public stativc void main (String [] args)  {
+public class test {
+    public static void main (String [] args)  {
         System.out.print("Test\n");
     }
 }
