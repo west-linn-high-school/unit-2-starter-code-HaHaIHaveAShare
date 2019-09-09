08d3376f6305502f6f6ced7a49df8b629663ff02
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2019-09-09 09:11:00 -0700

    Added geometry

---
+++ Geometry.java
@@ -0,0 +1,8 @@
+public class Geometry  {
+    public static void main (String [] args)  {
+        int sides = 7;
+        int radius = 5;
+        double tau = 6.28318;
+        System.out.printf("A heptagon has %d sides\nA decagon has 10 sides\nA dodecagon has 12 sides\nThe circumfrence of a circle with a radius of %d is %f\n", sides, radius, radius * tau);
+    }
+}
