082273718f6fd74e3d4672b7252271430759fb92
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2019-10-01 08:35:53 -0700

    Added Java Files

--- FXPractice.java
+++ FXPractice.java
@@ -8,23 +8,53 @@ import javafx.stage.Stage;
 
 public class FXPractice extends Application {
 
-  public void start(Stage stage){
-    Text hello = new Text(50, 50, "Hello World!");
-    Text question = new Text(120, 80, "How's it going?");
-    Rectangle box = new Rectangle(20, 20, 120, 50);
+    public void start(Stage stage){
+	Text hello = new Text(50, 50, "Hello World!");
+	Text question = new Text(120, 80, "How's it going?");
+	Rectangle box = new Rectangle(20, 20, 120, 50);
 
-    box.setStroke(Color.BLUE);
-    box.setFill(null);
+	box.setStroke(Color.BLUE);
+	box.setFill(null);
 
-    Group root = new Group(hello, question, box);
-    Scene scene = new Scene(root, 300, 120, Color.LIGHTGREEN);
+	Group root = new Group(hello, question, box);
+	Scene scene = new Scene(root, 300, 120, Color.LIGHTGREEN);
 
-    stage.setTitle("FXPractice");
-    stage.setScene(scene);
-    stage.show();
-  }
+	stage.setTitle("FXPractice");
+	stage.setScene(scene);
+	stage.show();
+    }
 
-  public static void main(String[] args){
-    launch(args);
-  }
+    public static void main(String[] args){
+	launch(args);
+    }
+}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class FXPractice extends Application {
+
+    public void start(Stage stage){
+	Text hello = new Text(50, 50, "Hello World!");
+	Text question = new Text(120, 80, "How's it going?");
+	Rectangle box = new Rectangle(20, 20, 120, 50);
+
+	box.setStroke(Color.BLUE);
+	box.setFill(null);
+
+	Group root = new Group(hello, question, box);
+	Scene scene = new Scene(root, 300, 120, Color.LIGHTGREEN);
+
+	stage.setTitle("FXPractice");
+	stage.setScene(scene);
+	stage.show();
+    }
+
+    public static void main(String[] args){
+	launch(args);
+    }
 }
---
+++ Snowmanning.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Snowman extends Application {
+
+    @Override
+    public void start(Stage stage){
+
+	double WIN_WIDTH = 640;
+	double WIN_HEIGHT = 480;
+	
+	Rectangle ground = new Rectangle(0, WIN_HEIGHT/2, WIN_WIDTH, WIN_HEIGHT/2);
+
+	ground.setFill(Color.WHITE);
+
+	Image img = new Image("https://i.pinimg.com/originals/11/1a/e8/111ae85eae5061d92e3b67de68516331.png", WIN_WIDTH * 10, WIN_HEIGHT * 10, false, true); // Work smarter, not harder
+        ImageView imgView = new ImageView(img);
+	
+	Group root = new Group(ground, imgView);
+	Scene scene = new Scene(root, WIN_WIDTH, WIN_HEIGHT, Color.LIGHTBLUE);
+      
+	stage.setTitle("\u2744\u2603\u2744\u2603\u2744\u2603\u2744");
+	stage.setScene(scene);
+	stage.show();
+	
+    }
+    public static void main(String[] args){
+	launch(args);
+    }
+}
